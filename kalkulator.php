deaec17ff03dc5bf5d298ecea63c41d766dbd0dc
fikri300301 <fp317572@gmai.com>
2022-07-07 21:13:31 +0700

    merubah nama

--- kalkulator.php
+++ kalkulator.php
@@ -31,44 +31,53 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Kalkulator</title>
-</head>
-<body>
 
-<form method="post">
-    <table align="center">
-      <tr>
-        <td align="center"><h1 colspan="3">KALKULATOR SEDERHANA</h1></td>
-      </tr>
-        <tr>
-            <td colspan="3"> <hr> </td>
-        </tr>
-        <tr>
-            <td>
-              <input type="text" name="angka1" value="<?= $angka1 ?>">
-              <input type="text" name="angka2" value="<?= $angka2  ?>">
-              <?php echo " = "  ?>
-              <input type="text" name="hasil" value="<?= $hasil ?>">
-            </td>
-        </tr>
-         <tr>
-            <td>
-              <input type="submit" name="tambah" value="+">
-              <input type="submit" name="kurang" value="-">
-              <input type="submit" name="bagi" value="/">
-              <input type="submit" name="kali" value="x">
-              <input type="submit" name="mod" value="mod">
-            </td>
-        </tr>
-        <tr>
-        <tr>
-            <td colspan="3"> <hr> </td>
-        </tr>
-    </table>
-</form>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kalkulator</title>
+</head>
+
+<body>
+    <h1>muhammad fikri pratama</h1>
+    <form method="post">
+        <table align="center">
+            <tr>
+                <td align="center">
+                    <h1 colspan="3">KALKULATOR SEDERHANA</h1>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="text" name="angka1" value="<?= $angka1 ?>">
+                    <input type="text" name="angka2" value="<?= $angka2  ?>">
+                    <?php echo " = "  ?>
+                    <input type="text" name="hasil" value="<?= $hasil ?>">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="tambah" value="+">
+                    <input type="submit" name="kurang" value="-">
+                    <input type="submit" name="bagi" value="/">
+                    <input type="submit" name="kali" value="x">
+                    <input type="submit" name="mod" value="mod">
+                </td>
+            </tr>
+            <tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+        </table>
+    </form>
 
 </body>
+
 </html>
